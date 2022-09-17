65560d406b15a998388f80a67420bf87b018506d
calin <calinoiu.alexandru@agilefreaks.com>
2022-09-17 21:34:03 +0300

    Add pagination for iOS

--- iosApp/iosApp/Explore/RepoListView.swift
+++ iosApp/iosApp/Explore/RepoListView.swift
@@ -7,20 +7,53 @@
 //
 
 import SwiftUI
+import ExploreShared
 
 struct RepoListView: View {
     @StateObject var viewModel = RepoListViewModel()
 
     var body: some View {
-        ForEach(self.viewModel.repoList, id: \.name) { repo in
-            VStack {
-                Text(repo.name)
-            }.padding(.bottom, 10)
-        }.onAppear {
-            Task {
-                await self.viewModel.loadRepos()
+        let list = self.viewModel.repoList
+        let isLastPage = self.viewModel.isLastPage
+
+        ZStack {
+            ScrollView(.vertical) {
+                LazyVStack {
+                    if !list.isEmpty {
+                        ListView(list)
+                    }
+                    
+                    if !isLastPage {
+                        LoadingView()
+                            .onAppear {
+                                Task {
+                                    await self.viewModel.loadRepos()
+                                }
+                            }
+                    }
+                }
             }
         }
+    }
+}
+
+private struct ListView: View {
+    private var list: [Repo]
+    
+    init(_ list: [Repo]) {
+        self.list = list
+    }
+    
+    var body: some View {
+        ForEach(list, id: \.name) { repo in
+            Text(repo.name)
+        }
+    }
+}
+
+private struct LoadingView: View {
+    var body: some View {
+        Text("Loading")
     }
 }
 
--- iosApp/iosApp/Explore/RepoListViewModel.swift
+++ iosApp/iosApp/Explore/RepoListViewModel.swift
@@ -13,17 +13,29 @@ import ExploreShared
 @MainActor final class RepoListViewModel: ObservableObject {
     @Published var repoList: [Repo] = []
     
+    let pageSize: Int32 = 40
+    
+    var isLastPage: Bool = false
+    var lastCursor: String? = nil
+    
     func loadRepos() async {
         let viewerRepo = RepositoryHelper().viewerRepository()
         
         do {
-            let result = try await viewerRepo.repos(pageSize: 10, after: nil)
+            let result = try await viewerRepo.repos(pageSize: pageSize, after: lastCursor)
             result.fold(
-                success: { (pagedResponse: SharedPagedResponse<Repo>?) -> Any? in
-                    self.repoList = pagedResponse?.response as! [Repo] },
+                success: { pagedResponse in self.handleSuccessResponse(pagedResponse: pagedResponse) },
                 failure: { (err: KotlinThrowable) -> () in print(err)} )
         } catch {
             print(error)
+        }
+    }
+    
+    private func handleSuccessResponse(pagedResponse: SharedPagedResponse<Repo>?) {
+        self.repoList.append(contentsOf: pagedResponse?.response as? [Repo] ?? [])
+        if let pageInfo = pagedResponse?.pageInfo {
+            self.lastCursor = pageInfo.endCursor
+            self.isLastPage = !pageInfo.hasNextPage
         }
     }
 }
