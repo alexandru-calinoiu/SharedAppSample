db60deb3b2f81d45039952bb51cd7c8d93252d98
ciprianhotea <cipryy_13@yahoo.com>
2022-09-21 11:21:31 +0300

    Implement Repo Details Screen

--- features/explore_shared/src/commonMain/kotlin/com/agilefreaks/sharedappsample/features/explore_shared/Explore.kt
+++ features/explore_shared/src/commonMain/kotlin/com/agilefreaks/sharedappsample/features/explore_shared/Explore.kt
@@ -1,6 +1,7 @@
 package com.agilefreaks.sharedappsample.features.explore_shared
 
+import com.agilefreaks.sharedappsample.features.explore_shared.repo_details.repoDetailsSharedModule
 import com.agilefreaks.sharedappsample.features.explore_shared.repo_list.repoListSharedModule
 import org.koin.core.module.Module
 
-fun modules() : List<Module> = listOf(repoListSharedModule)
+fun modules(): List<Module> = listOf(repoListSharedModule, repoDetailsSharedModule)
